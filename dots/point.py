29dd688bd27545965059d68d1047ff1bfb43aa17
giorgi_matcharashvili <giorgi.matcharashvili2002@gmail.com>
2022-12-12 13:33:23 +0100

    fix(point.py): fix distance to border finder error

--- dots/point.py
+++ dots/point.py
@@ -25,9 +25,12 @@ class Point(Base):
     @property
     def dist_to_border(self):
         """ returns the closest distance from the point to border """
-        border_points = [(self.RESOLUTION[0] / 2, self.pos[1]), (self.pos[0], self.RESOLUTION[1] / 2),
-                         (self.RESOLUTION[0] / 2, self.pos[1]), (self.pos[1], self.RESOLUTION[1] / 2)]
+        border_points = [
+            (self.pos[0], self.RESOLUTION[1] / 2),
+            (self.pos[0], -1 * (self.RESOLUTION[1] / 2)),
+            (self.RESOLUTION[0] / 2, self.pos[1]),
+            (-1 * (self.RESOLUTION[0] / 2), self.pos[1]),
+        ]
         closest_to_border = min([math.dist(self.pos, each) for each in border_points])
 
         return closest_to_border
-
